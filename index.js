0752cd77862a8c13e3559854fd61b0b7a3a2d4e6
IsaacLRubio <isaac.lopez@enroutesystems.com>
2018-05-10 12:23:33 -0500

    Multiple Channels Created

--- index.js
+++ index.js
@@ -6,14 +6,44 @@ firebase.initializeApp(config);
 
 let db = firebase.database();
 let ref = db.ref('messages/');
+let details = {};
+let userPath = '';
+let messagePath = '';
 let message = '';
+let user = '';
+let visitor = 1;
 
 ref.on('child_added', function(snapshot){
+    channel = `visitor-${visitor}`;
+    userPath = chat.getUserPath(snapshot);
     message = chat.getMessage(snapshot);
-    chat.sendMessage(message);
-})
+    user = chat.getUser(snapshot);
+    details = chat.getDetails(snapshot, message, channel);
+    chat.createChanel(channel).then(() => {
+        chat.sendMessage(channel, message.text, user);
+        ref.child(`${userPath}/${message.messagePath}`).update(details);
+        visitor++;
+    }).catch((err) => {
+        channel = chat.getChannel(snapshot);
+        chat.sendMessage(channel, message.text, user);
+        ref.child(`${userPath}/${message.messagePath}`).update(details);
+    });
+    }, function (errorObject) {
+    console.log("Initialzed Chat - there are no stored chats");
+});
 
 ref.on('child_changed', function(snapshot){
+    userPath = chat.getUserPath(snapshot);
     message = chat.getMessage(snapshot);
-    chat.sendMessage(message);
+    user = chat.getUser(snapshot);
+    channel = chat.getChannel(snapshot);
+    if(!snapshot.val()[message.messagePath]['channel'] && channel){
+        details = chat.getDetails(snapshot, message, channel);
+        chat.sendMessage(channel, message.text, user).catch((err) => {
+            chat.createChanel(channel).then(() => {
+                chat.sendMessage(channel, message.text, user);
+            });
+        });
+        ref.child(`${userPath}/${message.messagePath}`).update(details);
+    }
 });
--- src/chat.js
+++ src/chat.js
@@ -1,27 +1,66 @@
-const request = require('request-promise');
+const { WebClient } = require('@slack/client');
+
+const token = process.env.SLACKTOKEN;
+const web = new WebClient(token);
 
 var chat = {
-    getUser: function(snapshot){
-        return snapshot.key;
+    createChanel: function(channel){
+        return web.channels.create({name: channel, validate:true});
+    },
+
+    getChannel: function(obj){
+        let elements = obj.toJSON();
+        let messages = Object.keys(elements);
+        let messageKey = this.getPreviousMessagePath(messages);
+        if(messageKey){
+            return elements[messageKey]['channel'];
+        }
+        return null;
+    },
+
+    getDetails: function(snap, message, channel){
+        let currentState = snap.val()[message.messagePath]
+        currentState['channel'] = channel;
+        return currentState;
     },
 
     getMessage: function(obj){
         let elements = obj.toJSON();
         let messages = Object.keys(elements);
-        let messageKey = messages[messages.length-1];
-        return elements[messageKey]['message'];
+        let messageKey = this.getMessagePath(messages);
+        return {
+            text: elements[messageKey]['message'],
+            messagePath: messageKey
+        };
     },
-    
-    sendMessage: function(message){
-        return request({
-            url: process.env.SLACKAPI,
-            method: "POST",
-            json: {"text": message}
-        }).then(response => {
-            console.log("sendToSlack: successfully");
-        }).catch(error => {
-            console.log("sendToSlack: " + error);
+
+    getMessagePath: function(messages){
+        return messages[messages.length-1];
+    },
+
+    getPreviousMessagePath: function(messages){
+        if(messages.length>1) return messages[messages.length-2];
+        return null;
+    },
+
+    getUser: function(obj){
+        let elements = obj.toJSON();
+        let messages = Object.keys(elements);
+        let messageKey = this.getMessagePath(messages);
+        return elements[messageKey]['user'];
+    },
+
+    getUserPath: function(snap){
+        return snap.key;
+    },
+
+    sendMessage: function(channel, message, user){
+        return web.chat.postMessage({
+            channel: channel,
+            username: user,
+            text: message,
+            icon_emoji: ':squirrel:'
         });
-    }
+    },
 };
 module.exports = chat;
